28769641a69bee8bbe8cd95c5fe5ce8bcefe8a28
HARSHIL GURJAR <hdgurjar2323@gmail.com>
2026-02-04 17:14:50 +0530

    Add files via upload

---
+++ call_by_value.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+// Function Prototype
+void swap(int x, int y);
+
+
+int main()
+{
+    int a = 10, b = 20;
+
+    // Pass by Values
+    swapx(a, b);
+
+    printf("a=%d b=%d\n", a, b);
+
+    getch();
+}
+
+void swap(int x, int y)
+{
+    int t;
+
+    t = x;
+    x = y;
+    y = t;
+
+    printf("x=%d y=%d\n", x, y);
+}

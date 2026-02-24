a440f197583a296e0bb4cf11d32e8c67f50eaac5
HARSHIL GURJAR <hdgurjar2323@gmail.com>
2026-02-25 00:03:38 +0530

    Add files via upload

---
+++ if_else_condition.cpp
@@ -0,0 +1,20 @@
+#include <iostream>  
+#include<stdio.h>
+#include<conio.h>  
+   
+void main ()
+ {  
+	clrscr();
+	int n;  
+    	cout<<"Enter a Number: ";  
+   	 cin>>n;  
+           	 if (n % 2 == 0)    
+           	 {    
+                cout<<"It is even number"<<endl;    
+            	}   
+           	 else  
+            	{    
+                cout<<"It is odd number"<<endl;    
+            	}  
+   getch();  
+}  

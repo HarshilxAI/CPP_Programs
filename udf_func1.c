8d340356e2981bb43abb89e4daa03d984bcb7af3
HARSHIL GURJAR <hdgurjar2323@gmail.com>
2026-04-14 22:23:33 +0530

    Add files via upload

---
+++ udf_func1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+#include <iostream>  
+  
+void func() 
+{    
+	cout<<"function executed";    
+}    
+void main()  
+{  
+	clrscr();
+ 	func();    
+ 	func();    
+ 	func();    
+}  

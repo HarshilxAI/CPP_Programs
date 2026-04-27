7919a96840e9d5c0a61d5c888ddb486a682ab0ae
HARSHIL GURJAR <hdgurjar2323@gmail.com>
2026-04-28 00:12:32 +0530

    Add files via upload

---
+++ FOR_LOOP.C
@@ -0,0 +1,10 @@
+#include <iostream.h>
+void main() 
+{  
+          clrscr();
+         for(int i=1;i<=10;i++)
+         {      
+            cout<<i <<"\n";      
+          }
+	getch();       
+    }   
